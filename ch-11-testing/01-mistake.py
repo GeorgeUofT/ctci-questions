edf1ccf3ec916f215876d96abc3e5ee2890ac891
George Zywiel <g.zywiel@mail.utoronto.ca>
2018-02-05 14:36:08 -0500

    Removed solutions from all question files

--- ch-11-testing/01-mistake.py
+++ ch-11-testing/01-mistake.py
@@ -1,11 +1,2 @@
 # What was the mistake?
 
-def following_code():
-  i = 100
-  while i >= 0:
-    print("{}".format(i))
-    i -= 1
-
-if __name__ == "__main__":
-  following_code()
-
--- ch-16-moderate/06-smallest-difference.py
+++ ch-16-moderate/06-smallest-difference.py
@@ -10,39 +10,3 @@
 # This solution would continue by storing d items in a hash table for each
 # element in the first array.
 
-def smallest_difference(array1, array2):
-  if len(array1) == 0 or len(array2) == 0:
-    return None
-  sorted1, sorted2 = sorted(array1), sorted(array2)
-  smallest_diff = abs(sorted1[0] - sorted2[0])
-  ix1, ix2 = 0, 0
-  while True:
-    diff = sorted1[ix1] - sorted2[ix2]
-    smallest_diff = min(smallest_diff, abs(diff))
-    if diff == 0:
-      break
-    if diff < 0:
-      ix1 += 1
-      if ix1 == len(sorted1):
-        break
-    else:
-      ix2 += 1
-      if ix2 == len(sorted2):
-        break
-  return smallest_diff
-
-import unittest
-
-class Test(unittest.TestCase):
-  def test_smallest_difference(self):
-    self.assertEqual(smallest_difference([11, 22, 33, 44], [77, 2, 66, 50]), 6)
-    self.assertEqual(smallest_difference([11, 22, 33, 44], [77, 2, 34, 50]), 1)
-    self.assertEqual(smallest_difference([11, 77, 33, 44], [77, 2, 34, 50]), 0)
-    array1 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-    array2 = [33, 45, 58, 17]
-    self.assertEqual(smallest_difference(array1, array2), 2)
-    self.assertEqual(smallest_difference(array2, array1), 2)
-
-if __name__ == "__main__":
-  unittest.main()
-
